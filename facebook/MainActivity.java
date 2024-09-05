0170ef730c134c10b66938c66c34550a9141767b
Anan-Asharaf <144670411+Anan-Asharaf@users.noreply.github.com>
2024-09-05 12:40:46 +0530

    Add files via upload

---
+++ facebook/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.facebook;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.b1);
+
+        // Set up window insets listener to adjust padding for system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Set button click listener
+        button.setOnClickListener(this::b1);
+    }
+
+    public void b1(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/login"));
+        startActivity(browserIntent);
+    }
+}
